2221c1c5520d0e71e9d60f4e6520a2244028923e
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2011-04-08 12:22:26 +0100

    WL-1566 Fix citations/title view switching.
    Enabled noConflict on the citations view breaks the existing code on there. So we don't use noConflict.

--- citations-impl/impl/src/java/org/sakaiproject/citation/impl/CitationListAccessServlet.java
+++ citations-impl/impl/src/java/org/sakaiproject/citation/impl/CitationListAccessServlet.java
@@ -300,9 +300,6 @@ public class CitationListAccessServlet implements HttpAccess
 					+ "<link href=\"/library/skin/default/tool.css\" type=\"text/css\" rel=\"stylesheet\" media=\"all\" />\n"
 					+ "<script type=\"text/javascript\" src=\"/sakai-citations-tool/js/citationscript.js\"></script>\n"
 					+ "<script type=\"text/javascript\" src=\"/library/juice/jquery-1.3.2.min.js\"></script>\n"
-					+ "<script type=\"text/javascript\">\n"
-					+ "jQuery.noConflict();\n"
-					+ "</script>\n"
 					+ "<script type=\"text/javascript\" src=\"/library/juice/juice.js\"></script>\n"
 					+ "<script type=\"text/javascript\" src=\"/library/juice/juice-weblearn.js\"></script>\n"
 					+ "</head>\n<body>" );
