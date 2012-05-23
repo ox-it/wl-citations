6717ccc12e12a73f4407c9793a636af5126dc7d3
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-05-23 14:08:58 +0100

    Provide an URL for a citation

--- citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseCitationService.java
+++ citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseCitationService.java
@@ -4367,7 +4367,7 @@ public abstract class BaseCitationService implements CitationService
 	public String getEntityUrl(Reference ref)
 	{
 
-		return null;
+		return ref.getEntity().getUrl();
 	}
 
 	/*
